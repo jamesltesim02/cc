e1a6a230abfe8d16b6e35ea397df41a2dabb3b80
imheheda <imheheda@gmail.com>
2019-01-06 23:19:13 +0800

    添加coco自动任务到index

--- index.py
+++ index.py
@@ -4,27 +4,31 @@ from task.buyinCheckin import BuyinCheckin
 from task.Settlement import Settlement
 from task.CmsBuyinCheckin import CmsBuyinCheckin
 from task.CmsSettlement import CmsSettlement
+from task.CocoAutoTask import CocoAutoTask
 from config import config
 
-# 代理商自动审批任务, 每5秒执行一次
-# t = BuyinCheckin(8)
-# t.setApi(config)
-# t.start()
+# Ching自动审批任务, 每5秒执行一次
+t = BuyinCheckin(8)
+t.setApi(config)
+t.start()
 
-
-# 代理商自动结算任务, 每10秒执行一次
-# t1 = Settlement(10)
-# t1.setApi(config)
-# t1.start()
+# Ching自动结算任务, 每10秒执行一次
+t1 = Settlement(10)
+t1.setApi(config)
+t1.start()
 
 
 # CMS自动审批任务, 每5秒执行一次
 t2 = CmsBuyinCheckin(6)
 t2.setApi(config)
 t2.start()
-# t2.callback()
 
 # CMS代理商自动结算任务, 每10秒执行一次
-# t3 = CmsSettlement(10)
-# t3.setApi(config)
-# t3.start()
+t3 = CmsSettlement(10)
+t3.setApi(config)
+t3.start()
+
+# Coco自动审批与结算任务，每8秒执行一次
+t3 = CocoAutoTask(8)
+t3.setApi(config)
+t3.start()
